b736637890dc117dbf00b8e26e828f51eba073cc
mcintdre000 <38018234+mcintdre000@users.noreply.github.com>
2018-06-26 20:39:03 -0700

    Added reusable HOC

--- src/components/Admin/AdminData.js
+++ src/components/Admin/AdminData.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import { arrayToArrayofObject } from '../../utils/functions';
+// import axios from 'axios';
+// import { arrayToArrayofObject } from '../../utils/functions';
 import '../../Styling/Admin.css';
 import { withData } from './AdminDataContainer';
 
